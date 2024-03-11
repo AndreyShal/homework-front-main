15d969ad2a205ff6c176ad520810d4098158390a
andreyshalomitsky <andreyshalomitsky@gmail.com>
2024-03-11 10:33:01 +0500

    created homework 04-02 hw10

--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -2,10 +2,12 @@ const initState = {
     isLoading: false,
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export const loadingReducer = (state: LoadingType = initState, action: LoadingActionType): LoadingType => { // fix any
     switch (action.type) {
         // пишет студент  // need to fix
-
+        case "CHANGE_LOADING": {
+            return {...state, isLoading: action.isLoading}
+        }
         default:
             return state
     }
@@ -20,3 +22,9 @@ export const loadingAC = (isLoading: boolean): LoadingActionType => ({
     type: 'CHANGE_LOADING',
     isLoading,
 })
+
+//types
+
+type LoadingType = {
+    isLoading: boolean,
+}
